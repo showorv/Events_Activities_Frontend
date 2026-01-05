0309fc921cf81341fadf91f75d20a144b1b22349
Showrov <yshowrov7@gmail.com>
2026-01-06 02:56:23 +0600

    redeploy

--- src/components/modules/user/BecomeHost.tsx
+++ src/components/modules/user/BecomeHost.tsx
@@ -10,7 +10,7 @@ export default function BecomeHostClient() {
     try {
       setLoading(true);
 
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/become-host`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/user/become-host`, {
         method: "PATCH",
         credentials: "include",
       });
